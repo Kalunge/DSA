ab08b7c0b4c038c76c9283aae30e9c0ecb46973c
Kalungez <muthomititus+github@gmail.com>
2020-08-17 11:46:56 +0300

    add array chunk

--- testing.js
+++ testing.js
@@ -7,6 +7,9 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+
+
 // 2. PALINDROME
 // --- Directions
 // Given a string, return true if the string is a palindrome
@@ -14,8 +17,7 @@
 // form the same word if it is reversed. *Do* include spaces
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+
 
 
 
@@ -30,14 +32,47 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(num) {
-  let reversed = num.toString().split('').reverse().join('')
 
-  return parseInt(reversed) * Math.sign(num)
-}
 
-console.log(reverseInt(15))
-console.log(reverseInt(981))
-console.log(reverseInt(500))
-console.log(reverseInt(-15))
-console.log(reverseInt(-90))
+// 4. MAXCHAR
+// 4. MAXCHAR
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+
+
+
+// 5. FIZZUBUZZ
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+
+
+// 6. ARRY CHUNKS
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+
